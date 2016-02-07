c3da77c34414923da6798454fcb731195d521216
cervoise <antoine.cervoise@gmail.com>
2016-02-07 20:06:23 +0100

    Update HBF2.ino

--- Arduino/HBF2/HBF2.ino
+++ Arduino/HBF2/HBF2.ino
@@ -1,3 +1,58 @@
-void setup() {}
+#if defined(__AVR_ATmega32U4__)
+  #include <Mouse.h>
+  #include <Keyboard.h>
+#endif
 
-void loop() {}
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x04
+int number = 0;
+ 
+void setup() {
+
+ //Starting keyboard
+ keyboardStart();
+
+ // initialize i2c as slave
+ Wire.begin(SLAVE_ADDRESS);
+ 
+ // define callbacks for i2c communication
+ Wire.onReceive(receiveData);
+ Wire.onRequest(sendData);
+}
+ 
+void loop() {
+ delay(100);
+}
+ 
+// callback for received data
+void receiveData(int byteCount){
+ 
+ while(Wire.available()) {
+  number = Wire.read();
+  if (number > 31) {
+    typeKey(number);
+  }
+  else {
+    switch (number) {
+      case 8:
+        typeBackspace();
+        break;
+      case 9:
+        typeTab();
+        break;
+      case 13:
+        typeEnter();
+        break;
+      case 27:
+        typeEscape();
+        break;
+    }
+  }
+ }
+}
+ 
+// callback for sending data
+void sendData(){
+ Wire.write(number);
+}
